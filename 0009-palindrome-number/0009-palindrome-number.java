9ce80980c4a2bc4c62dc6d16e051f81162416a5f
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-07-21 21:28:14 +0530

    Time: 5 ms (80.76%), Space: 46.3 MB (6.45%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        int temp = x;
+        int result = 0;
+        while(x > 0) {
+            int rem = x%10;
+            result = result*10 + rem;
+            x /= 10;
+        }
+        return (temp == result) ? true : false;
+    }
+}
