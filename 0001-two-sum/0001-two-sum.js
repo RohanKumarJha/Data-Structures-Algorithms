e222f63b37b7b0201db07f79236953597d1f403e
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-08-22 09:32:57 +0530

    Time: 2 ms (70.19%), Space: 56.8 MB (11.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,9 +1,8 @@
 var twoSum = function(nums, target) {
     const map = new Map();
     for(let i=0; i<nums.length; i++) {
-        const complement = target - nums[i];
-        if(map.has(complement)) {
-            return [map.get(complement), i];
+        if(map.has(target-nums[i])) {
+            return [map.get(target-nums[i]),i];
         }
         map.set(nums[i],i);
     }
