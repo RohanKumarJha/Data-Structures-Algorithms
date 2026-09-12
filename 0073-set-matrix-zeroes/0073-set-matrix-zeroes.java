32152b4cd46cd8169c4ab62b56b2aaa7e1948b9b
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-09-12 09:11:58 +0530

    Time: 1 ms (96.21%), Space: 47.6 MB (42.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,23 +3,58 @@ class Solution {
         int row = matrix.length;
         int col = matrix[0].length;
 
-        int[] row_matrix = new int[row];
-        int[] col_matrix = new int[col];
+        boolean flag = (matrix[0][0] == 0) ? true : false;
+        boolean row_flag = false;
+        boolean col_flag = false;
 
         for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
+            if(matrix[i][0] == 0) {
+                row_flag = true;
+                break;
+            }
+        }
+
+        for(int i=0; i<col; i++) {
+            if(matrix[0][i] == 0) {
+                col_flag = true;
+                break;
+            }
+        }
+
+        for(int i=1; i<row; i++) {
+            for(int j=1; j<col; j++) {
                 if(matrix[i][j] == 0) {
-                    row_matrix[i] = 1;
-                    col_matrix[j] = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-                if(row_matrix[i]==1 || col_matrix[j]==1) {
+        for(int i=1; i<row; i++) {
+            for(int j=1; j<col; j++) {
+                if(matrix[i][0]==0 || matrix[0][j]==0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        if(flag == true) {
+            for(int i=0; i<row; i++) {
+                matrix[i][0] = 0;
+            }
+            for(int i=0; i<col; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(row_flag == true) {
+            for(int i=0; i<row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if(col_flag == true) {
+            for(int i=0; i<col; i++) {
+                matrix[0][i] = 0;
             }
         }
     }
