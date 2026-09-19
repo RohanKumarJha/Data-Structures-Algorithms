e25539e3255e402a0e0362fc3633ed82c80df329
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-09-20 01:52:24 +0530

    Time: 0 ms (100%), Space: 48.1 MB (87.87%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    private void swap(char[] s,int start,int end) {
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+    }
+    
+    public void reverseString(char[] s) {
+        int start=0, end=s.length-1;
+        while(start < end) {
+            swap(s,start,end);
+            start++;
+            end--;
+        }
+    }
+}
